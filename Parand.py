d08dcb87102daa228203fe50ef02d7c7e802646f
paulsichone90 <paulsichone000@gmail.com>
2026-04-16 22:58:31 +0200

    Replace array with list for character storage

--- Parand.py
+++ Parand.py
@@ -1,9 +1,8 @@
-from array import *
 # ACCEPT USER INPUT
 word = input("Enter a world or phrase: ")
 word = word.lower().strip().replace(" ", "")
 # CREATE AN ARRAY OF CHARACTERS
-char = array('u', word)
+char = list( word)
 reversed_char = char.reverse()
 if char == reversed_char :
     print(f"{word} is a Palindrome")
